3de7347dfe1dc4dc2317a16694049c34b311a07d
pablo-aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2023-12-05 14:54:59 +0100

    hello world dell'app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import {globalStyles} from "../styles/global";
+
+export default function App() {
+  return (
+      <View style={globalStyles.container}>
+        <Text>init app!</Text>
+      </View>
+  );
+}
