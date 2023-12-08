c23a76ffdc07f274befc05d2cd0259bd524d73b7
pablo-aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2023-12-08 12:58:28 +0100

    sid hardcoded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {View} from 'react-native';
 import {globalStyles} from "../styles/global";
 import RankingScreen from "./screens/RankingScreen";
+import {useState} from "react";
+import AppViewModel from "./viewmodels/AppViewModel";
 
 export default function App() {
-  return (
-      <View style={globalStyles.container}>
-        <RankingScreen />
-      </View>
-  );
+    const [viewModel] = useState(new AppViewModel())
+
+    return (
+        <View style={globalStyles.container}>
+            <RankingScreen sid={viewModel.getSid()}/>
+        </View>
+    );
 }
---
+++ src/viewmodels/AppViewModel.js
@@ -0,0 +1,6 @@
+export default class AppViewModel {
+
+    getSid() {
+        return 'MOVZg1fdqSXRsMWwnnFY';
+    }
+}
