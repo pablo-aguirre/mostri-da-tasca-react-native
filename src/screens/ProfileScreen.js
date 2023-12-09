449b5ce66c3b8087e6da756790ae5d585eb0d8e6
pablo-aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2023-12-09 10:44:25 +0100

    elenco di punti da fare

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,9 +1,27 @@
 import {Text} from "@rneui/themed";
+import {View} from "react-native";
 
 export default function ProfileScreen({sid}) {
     return (
-        <Text>
-            ProfileScreen
-        </Text>
+        <View>
+            <Text>
+                Nome utente (modificabile)
+            </Text>
+            <Text>
+                Immagine del profilo (modificabile)
+            </Text>
+            <Text>
+                Posizione condivisa (booleano modificabile)
+            </Text>
+            <Text>
+                Punti vita
+            </Text>
+            <Text>
+                Punti esperienza
+            </Text>
+            <Text>
+                ARTEFATTI
+            </Text>
+        </View>
     )
 }
