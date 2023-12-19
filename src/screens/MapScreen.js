8a679c7a090566d8af0a32007325b23dc3fc3ebf
pablo-aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2023-12-19 09:49:32 +0100

    primissima versione con oggetti virtuali nelle vicinanze con realtive informazioni se cliccati

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,44 +1,151 @@
-import React, {useEffect, useState} from "react";
-
+import React, {createContext, useContext, useEffect, useState} from "react";
+import {Appbar, Button, Dialog, Icon, List, Portal, Text} from "react-native-paper";
 import * as Location from 'expo-location';
-import MapView from 'react-native-maps'
-import {Appbar} from "react-native-paper";
+import MapView, {Marker} from "react-native-maps";
+import CommunicationController from "../models/CommunicationController";
+import {SessionID} from "../Contexts";
+import {iconsObjects, ObjectAvatar} from "../components/ObjectAvatar";
+
+const MapContext = createContext()
 
 export function MapScreen() {
-    const [location, setLocation] = useState()
-    const [region, setRegion] = useState()
+    const [currentLocation, setCurrentLocation] = useState()
+    const [dialogVisible, setDialogVisible] = useState(false)
+    const [selectedObject, setSelectedObject] = useState(undefined)
 
     useEffect(() => {
-        Location.requestForegroundPermissionsAsync()
-            .then(result => console.log(`[MapScreen] permissions: ${JSON.stringify(result)}`))
-        Location.watchPositionAsync(
-            {
-                accuracy: Location.Accuracy.High,
-                distanceInterval: 1
-            },
-            (newLocation) => {
-                console.log(`[MapScreen] new location: ${JSON.stringify(newLocation)}`)
-                setLocation(newLocation.coords)
-            }
-        )
+        const periodicPosition = async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync()
+            if (status === 'granted')
+                Location.watchPositionAsync(
+                    {
+                        accuracy: Location.Accuracy.Balanced,
+                        distanceInterval: 10
+                    },
+                    (newLocation) => {
+                        setCurrentLocation({
+                            lat: newLocation.coords.latitude,
+                            lon: newLocation.coords.longitude
+                        })
+                    }
+                )
+        }
+        periodicPosition()
     }, []);
 
-    useEffect(() => {
-        setRegion({...location, latitudeDelta: 0.005, longitudeDelta: 0.005})
-    }, [location]);
-
-    console.log(location);
-    console.log(region);
-    return (
-        <>
+    return (currentLocation &&
+        <MapContext.Provider
+            value={{currentLocation, dialogVisible, setDialogVisible, selectedObject, setSelectedObject}}>
             <Appbar.Header mode='small' elevated>
                 <Appbar.Content title="Map"/>
             </Appbar.Header>
-            <MapView
-                showsUserLocation
-                region={region}
-                style={{width: '100%', height: '86%'}}
-            />
-        </>
+            {selectedObject && <ObjectDetail/>}
+            <MyMap/>
+        </MapContext.Provider>
+    )
+}
+
+function MyMap() {
+    const {currentLocation} = useContext(MapContext)
+    const sid = useContext(SessionID)
+    const [region, setRegion] = useState({
+        latitude: currentLocation.lat,
+        longitude: currentLocation.lon,
+        latitudeDelta: 0.0004,
+        longitudeDelta: 0.0004
+    })
+    const [objects, setObjects] = useState([])
+
+    useEffect(() => {
+        setRegion({
+            ...region,
+            latitude: currentLocation.lat,
+            longitude: currentLocation.lon
+        })
+        CommunicationController.nearbyObjects(sid, currentLocation.lat, currentLocation.lon)
+            .then((result) => setObjects(result))
+    }, [currentLocation])
+
+    return (
+        <MapView
+            style={{width: '100%', height: '86%'}}
+            provider={"google"}
+            showsUserLocation
+            //scrollEnabled={false}
+            //minZoomLevel={15}
+            showsMyLocationButton={true}
+            region={region}
+            onRegionChangeComplete={(newRegion) => setRegion(newRegion)}
+        >
+            {objects.map((object) => <MyMarker virtualObject={object}/>)}
+        </MapView>
+    )
+}
+
+function MyMarker({virtualObject}) {
+    const sid = useContext(SessionID)
+    const {setDialogVisible, setSelectedObject} = useContext(MapContext)
+
+    const [details, setDetails] = useState()
+
+    useEffect(() => {
+        CommunicationController.objectInformation(sid, virtualObject.id)
+            .then(result => setDetails(result))
+    }, []);
+
+    console.log(details);
+    return (details &&
+        <Marker
+            key={details.id}
+            coordinate={{latitude: details.lat, longitude: details.lon}}
+            onPress={() => {
+                setSelectedObject(details)
+                setDialogVisible(true)
+            }}
+        >
+            <Icon size={40} source={iconsObjects[details.type]}/>
+        </Marker>
+    )
+}
+
+function ObjectDetail() {
+    const {dialogVisible, setDialogVisible, selectedObject, setSelectedObject} = useContext(MapContext)
+
+    function isActivable(selectedObject) {
+        // TODO implementare logica "a portata di mano" considerando il possesso di un amuleto
+        return false
+    }
+
+    return (
+        <Portal>
+            <Dialog visible={dialogVisible} onDismiss={() => {
+                setSelectedObject(undefined)
+                setDialogVisible(false)
+            }}>
+                <Dialog.Title style={{textAlign: "center"}}>{selectedObject.name}</Dialog.Title>
+                <Dialog.Content>
+                    <ObjectAvatar object={selectedObject} large/>
+                    <List.Item
+                        left={() => <List.Icon icon={'ab-testing'}/>}
+                        title={'Type'}
+                        right={() => <Text>{selectedObject.type}</Text>}
+                    />
+                    <List.Item
+                        left={() => <List.Icon icon={'chevron-double-up'}/>}
+                        title={'Level'}
+                        right={() => <Text>{selectedObject.level}</Text>}
+                    />
+                    {selectedObject.type === "monster" &&
+                        <Text>
+                            Potresti perdere vita
+                        </Text>
+                    }
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <Button onPress={() => setDialogVisible(false)}>Cancel</Button>
+                    <Button onPress={() => console.log('Cancel')} disabled={!isActivable(selectedObject)}>Activate</Button>
+                </Dialog.Actions>
+            </Dialog>
+        </Portal>
     )
 }
